b2d9633722b96f7d31f6b19b53582f557855bdfd
taftafa <TafTafa@gmail.com>
2015-04-01 01:26:11 +0400

    AML-79:WikiParserWithoutDots

---
+++ links.py
@@ -0,0 +1,24 @@
+path = "C:\\Users\\Toshik\\AML\\wikipedia_links_en.nt"
+datafile = open(path, 'r')
+link = dict([])
+dataall = []
+
+for i in range(1, 100000):
+    data = datafile.readline()
+    dataset = data.split(' ')
+    if (dataset[0].find("http://en.wikipedia.org/wiki/Benjamin_Tucker")!= -1 and dataset[2].find("dbpedia.org") != -1):
+        dataall.append(dataset[2])
+
+path = "C:\\Users\\Toshik\\AML\\instance_types_en.nt"
+
+print dataall
+
+datafile = open(path, 'r')
+datalen = len(datafile.readlines())
+print datalen
+data = datafile.readline()
+while (data.__len__() != 0):
+    dataset = data.split(' ')
+    if dataall.count(dataset[0]) != 0:
+        print dataset[2]
+    data = datafile.readline()
