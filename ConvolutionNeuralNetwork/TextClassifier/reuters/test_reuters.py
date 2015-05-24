34b54c444051de16a36c1e7cb37c9927d517db28
Гольцман Ирина Анатольевна <irina-goltsman@phystech.edu>
2015-05-24 08:28:50 +0300

    Хороший формат для сборки в бамбу

--- ConvolutionNeuralNetwork/TextClassifier/reuters/test_reuters.py
+++ ConvolutionNeuralNetwork/TextClassifier/reuters/test_reuters.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import unittest
+import xmlrunner
 import sys
 import numpy as np
 sys.path.insert(0, '..')
@@ -13,6 +15,9 @@ import re
 sys.path.insert(0, '../../../AML-TextClassification')
 import reuters
 from sklearn import metrics
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 def text_to_wordlist(text, remove_stopwords=False):
@@ -61,51 +66,60 @@ def get_word2vec_model(X_train, X_test):
     return model
 
 
-def main():
-    print "Loading and parsing data..."
-    data_path = '../../../AML-TextClassification/data'
-    rp = reuters.ReutersParser(data_path, multilabel=False)
-    rp.parse()
-    X_train = rp.get_corpus("train", "topics", "data")
-    Y_train = rp.get_corpus("train", "topics", "target")
+class TestReutersTextClassifier(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """ Загрузка данных, загрузка заранее обученного классификатора """
+        print "Loading and parsing data..."
+        data_path = '../../../AML-TextClassification/data'
+        rp = reuters.ReutersParser(data_path, multilabel=False)
+        rp.parse()
+        cls.X_train = rp.get_corpus("train", "topics", "data")
+        cls.Y_train = rp.get_corpus("train", "topics", "target")
 
-    X_test = rp.get_corpus("test", "topics", "data")
-    Y_test = rp.get_corpus("test", "topics", "target")
-    print "OK"
+        cls.X_test = rp.get_corpus("test", "topics", "data")
+        cls.Y_test = rp.get_corpus("test", "topics", "target")
+        print "OK"
+        n_out = max(cls.Y_train) + 1
+        #model_name = "word2vec_100_reuters"
+        #model = get_word2vec_model(X_train, X_test)
+        #model.save(model_name)
+        model_path = "../100features_40minwords_10context"
 
-    n_out = max(Y_train) + 1
-    #model_name = "word2vec_100_reuters"
-    #model = get_word2vec_model(X_train, X_test)
-    #model.save(model_name)
-    model_path = "../100features_40minwords_10context"
-    # learning_rate=0.1, window=5, n_hidden=20, n_filters=25,
-    #                                 n_out=n_out, word_dimension=400
-    cnn_text_clf = CNNTextClassifier(learning_rate=0.1, window=5, n_hidden=10, n_filters=25,
-                                     n_out=n_out, word_dimension=100, seed=1,
-                                     model_path=model_path, L1_reg=0.1, L2_reg=0.1)
-    cnn_text_clf.ready()
-    # print "test score before training:", cnn_text_clf.score(X_test, Y_test)
+        cls.cnn_text_clf = CNNTextClassifier(learning_rate=0.1, window=5, n_hidden=10,
+                                             n_filters=25,
+                                             n_out=n_out, word_dimension=100, seed=1,
+                                             model_path=model_path, L1_reg=0.1, L2_reg=0.1)
 
-    new_state_path = 'cnn_best_100_state_2015-05-24-05:42:00'
-    print "Loading state for classifier..."
-    cnn_text_clf.load(new_state_path)
-    print "Count score..."
-    print "result =", np.mean(cnn_text_clf.predict(X_test) == Y_test)
-    print "precision =", metrics.precision_score(Y_test, cnn_text_clf.predict(X_test))
-'''
-    try:
-        cnn_text_clf.fit(X_train, Y_train, X_test[0:500], Y_test[0:500], n_epochs=50)
-    except KeyboardInterrupt:
-        print "Fit Interrupt, if you really want to interrupt execution, try again"
+        new_state_path = 'cnn_best_100_state_2015-05-24-05:42:00'
+        print "Loading state for classifier..."
+        cls.cnn_text_clf.load(new_state_path)
 
-    new_state_path = "cnn_best_100_state_" + datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
-    print "Saving state to '%s'..." % new_state_path
-    cnn_text_clf.save_state(new_state_path)
+    def test_dimensions(self):
+        params = self.cnn_text_clf.get_cnn_params()
+        self.assertEqual(len(params), 6)
+        # Вектор смещения для первого свёрточого слоя
+        self.assertEqual(len(params[5].get_value()), self.cnn_text_clf.n_filters)
+        # Веса для первого свёрточого слоя
+        self.assertEqual(params[4].get_value().shape, (self.cnn_text_clf.n_filters, 1,
+                                                       self.cnn_text_clf.window,
+                                                       self.cnn_text_clf.word_dimension))
+        # Вектор смещения для полносвязного слоя
+        self.assertEqual(params[3].get_value().shape, (self.cnn_text_clf.n_hidden,))
+        # Веса для полносвязного слоя
+        self.assertEqual(params[2].get_value().shape, (self.cnn_text_clf.n_filters,
+                                                       self.cnn_text_clf.n_hidden))
 
-    print "TOTAL TEST SCORE:", cnn_text_clf.score(X_test, Y_test)
-    metrics.precision_score(Y_test, cnn_text_clf.predict(X_test))
-'''
+    def test_score(self):
+        print "Count score..."
+        score = np.mean(self.cnn_text_clf.predict(self.X_test) == self.Y_test)
+        print "result =", score
+        precision = metrics.precision_score(self.Y_test, self.cnn_text_clf.predict(self.X_test))
+        print "precision =", precision
+        self.assertTrue(score > 0.8146)
+        self.assertTrue(precision > 0.75)
+
+
 if __name__ == '__main__':
-    start_time = time.time()
-    main()
-    print("--- %s seconds ---" % (time.time() - start_time))
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+
